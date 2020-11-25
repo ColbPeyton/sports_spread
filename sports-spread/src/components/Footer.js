b10d82b6617b50eb0976f4a6ef3fc3a588addcb1
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2020-11-25 01:04:50 -0600

    Line styling almost completed
    Updated ChoicePage to fit new line styling. Will add two columns of lines at 768px and 3 columns at 1200px

--- sports-spread/src/components/Footer.js
+++ sports-spread/src/components/Footer.js
@@ -2,14 +2,23 @@ import React from 'react';
 
 import '../styles/Footer.scss';
 
+const dev = 'https://www.colbypeyton.com/';
+
 
 function Footer(props){
 
     return(
         <div className='footer'>
             <div className='footer-container'>
-                footer
+                <ul>
+                    <li>Lorem</li>
+                    <li>Lorem</li>
+                </ul>
+                <div className='dev'>
+                    <p>Developed by <a href={dev} target="_blank" rel="noreferrer">ColbPeyton</a></p>
+                </div>
             </div>
+
         </div>
    
     );
