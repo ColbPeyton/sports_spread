affd05370ce22428b5628a07bd6ab094feb66a22
Colby Peyton <colby.peyton@gmail.com>
2020-11-26 01:52:22 -0600

    Added ScrollToTop from previous projects. Added content/styling to HomeScreen

--- sports-spread/src/App.js
+++ sports-spread/src/App.js
@@ -14,6 +14,7 @@ import RecordPage from './components/pages/ChoicePage';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 const routes = [
   <NavLink exact className='nav' activeClassName="selected" to="/">Home</NavLink>,
@@ -43,6 +44,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+      <ScrollToTop />
         <Header routes={routes} width={width}/>
 
         <Switch>
--- sports-spread/src/components/pages/HomePage.js
+++ sports-spread/src/components/pages/HomePage.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
+import '../../styles/HomePage.scss';
 
+import badge from '../../assets/icons/hoops_badge.png'
 function HomePage(){
     return(
         <div className='home-page'>
-                home
+            <div className='home-container'>
+                <div className='badge'>
+                    <img src={badge} alt='Dr.Hoops Badge' />
+                </div>
+                <div className='btn-container'>
+                    <NavLink exact className='link' to='/picks'>
+                        <p>See Today's Picks</p>
+                    </NavLink>
+                </div>
+            </div>
         </div>
     );
 }
