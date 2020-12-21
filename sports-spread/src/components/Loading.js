99b47f222257dd03b5b0ca3a32618f3436add69d
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2020-12-21 11:53:07 -0600

    Created Loading component and stylesheet
    Animation/component will be displayed while data is pulled from DB.

---
+++ sports-spread/src/components/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../styles/Loading.scss';
+
+function Loading(props){
+    return(
+        <div className='loading'>
+
+        </div>
+    )
+}
+
+
+export default Loading;
